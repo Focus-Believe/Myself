e27caca7b60e64f91f72a251f172e9347ea7513e
Focus-Believe <focusandbelieve50@gmail.com>
2026-04-27 23:43:48 +0600

    main.py

--- main.py
+++ main.py
@@ -1,98 +1,99 @@
+import json, os
+from datetime import datetime
 import uvicorn
-import json
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
-from db import DataB
-app=FastAPI()
-db=DataB()
-clients={}
-html='''
-<h1>CHAT APP</h1>
-<h4>UPDATE</h4>
-<input id ='name' placeholder ='Enter Name'>
-<br>
-<input id ='msg' placeholder ='Write Message'>
-<br>
-<button onclick='send()'>Send</button>
-<br>
-<h3> User List </h3>
-<ul id ='user'></ul>
-<h3>Message </h3>
-<ul id ='chat'></ul>
-<script>
-let ws = new WebSocket(
-  (location.protocol === 'https:' ? 'wss://' : 'ws://') + location.host + '/ws'
-);
-function send(){
-let user=document.getElementById('name').value;
-let message =document.getElementById('msg').value;
-ws.send(JSON.stringify({
-name:user,
-msg:message}));
-document.getElementById('msg').value='';
-}
-ws.onmessage=function(event){
-let data=JSON.parse(event.data);
-if(data.type==='users'){
-let ul=document.getElementById('user');
-ul.innerHTML='';
-data.data.forEach(u=>{
-let li=document.createElement('li');
-li.textContent=u;
-ul.appendChild(li);})
-}
-if(data.type ==='message'){
-let li=document.createElement('li');
-li.textContent= data.name + ':' + data.msg;
-document.getElementById('chat').appendChild(li);
-}}
-</script>
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
-'''
-@app.get('/')
+from db import DB
+from manager import Manager
+
+app = FastAPI()
+db = DB()
+mgr = Manager()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
 def home():
-	return HTMLResponse(html)
-@app.websocket('/ws')
-async def webb(websocket: WebSocket):
-	await websocket.accept()
-	try:
-		while True:
-			data =await websocket.receive_text()
-			mst=json.loads(data)
-			name=mst.get('name')
-			msg=mst.get('msg')
-			if websocket not in clients and name:
-				clients[websocket]=name
-				await svup()
-				rows=db.sh()
-				for row in rows:
-					await websocket.send_text(json.dumps({
-					'type': 'message',
-					'name': row[0],
-					'msg': row[1]}))
-			if name and msg:
-				db.sv(name, msg)
-				await brodc({'type': 'message',
-				'name': name,
-				'msg': msg})
-	except WebSocketDisconnect:
-		clients.pop(websocket, None)
-		await svup()
-async def brodc(message):
-	for client in list(clients):
-		try:
-			await client.send_text(json.dumps(message))
-		except:
-			clients.pop(client, None)
+    return FileResponse("templates/index.html")
 
-async def svup():
-	name=list(clients.values())
-	for client in list(clients):
-		try:
-			await client.send_text(json.dumps({'type': 'users',
-			'data': name}))
-		except:
-			clients.pop(client, None)
-		
+
+async def send_users():
+    users = mgr.users()
+    for ws in mgr.name_to_ws.values():
+        await ws.send_text(json.dumps({
+            "type": "users",
+            "data": users
+        }))
+
+
+@app.websocket("/ws")
+async def ws(websocket: WebSocket):
+    await websocket.accept()
+
+    try:
+        while True:
+            data = json.loads(await websocket.receive_text())
+            t = data["type"]
+
+            # REGISTER
+            if t == "register":
+                ok = db.register(data["name"], data["password"])
+                await websocket.send_text(json.dumps({
+                    "type": "register_ok" if ok else "register_fail"
+                }))
+
+            # LOGIN
+            elif t == "login":
+                ok = db.login(data["name"], data["password"])
+
+                if ok:
+                    await mgr.connect(data["name"], websocket)
+                    await send_users()
+
+                await websocket.send_text(json.dumps({
+                    "type": "login_ok" if ok else "login_fail"
+                }))
+
+            # DM (FAST O(1))
+            elif t == "dm":
+                sender = mgr.get_name(websocket)
+                target = mgr.get_ws(data["to"])
+                time = datetime.now().strftime("%H:%M")
+
+                db.save_msg(sender, data["to"], None, data["msg"], time)
+
+                if target:
+                    await target.send_text(json.dumps({
+                        "type": "msg",
+                        "from": sender,
+                        "msg": data["msg"],
+                        "time": time
+                    }))
+
+            # ROOM
+            elif t == "room":
+                sender = mgr.get_name(websocket)
+                time = datetime.now().strftime("%H:%M")
+
+                db.save_msg(sender, None, data["room"], data["msg"], time)
+
+                msg = json.dumps({
+                    "type": "room",
+                    "from": sender,
+                    "msg": data["msg"],
+                    "time": time
+                })
+
+                for ws in mgr.name_to_ws.values():
+                    await ws.send_text(msg)
+
+    except WebSocketDisconnect:
+        mgr.disconnect(websocket)
+        await send_users()
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
